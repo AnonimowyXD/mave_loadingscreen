47777aeac6f63429a6d9f716acba4aa5cdfbd432
Januszek <46579570+AnonimowyXD@users.noreply.github.com>
2024-01-20 22:13:29 +0100

    Add files via upload

---
+++ mave_loadingscreen/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+  'index.html',
+  'css/keks.css',
+  'css/vegas.min.css',
+  'config.js',
+  'js/vegas.min.js',
+}
+
+loadscreen 'index.html'
